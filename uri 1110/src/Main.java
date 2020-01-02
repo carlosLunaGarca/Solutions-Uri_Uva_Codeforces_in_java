ea7cce3502ffd890b839f47266d4737f272f9d96
carlos <carlos@carlos-X441NA>
2020-01-02 14:37:01 -0500

    reaoganizar

--- uri 1110/src/Main.java
+++ uri 1110/src/Main.java
@@ -4,27 +4,28 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+
 public class Main {
 
-	public static void main(String[] args)throws IOException {
-	  BufferedReader in =new BufferedReader(new InputStreamReader(System.in));
-	  while (true) {
-	  int n=Integer.parseInt(in.readLine());
-	  if(n<=0)
-		  break;
-      Queue<Integer> cola=new LinkedList<Integer>();
-      for (int i = 0; i < n; i++) {
-       cola.add(i+1);
-	}
-      int[] vec=new int[n-1];
-      int i=0;
-      while(cola.size()!=1) {
-    	  vec[i]=cola.poll();
-    	  cola.add(cola.poll());
-    	  i++;
-      }
-      System.out.println("Discarded cards:"+Arrays.toString(vec).replaceAll("\\["," ").replaceAll("\\]",""));
-      System.out.println("Remaining card: "+cola.peek());
-	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			int n = Integer.parseInt(in.readLine());
+			if (n <= 0)
+				break;
+			Queue<Integer> cola = new LinkedList<Integer>();
+			for (int i = 0; i < n; i++) {
+				cola.add(i + 1);
+			}
+			int[] vec = new int[n - 1];
+			int i = 0;
+			while (cola.size() != 1) {
+				vec[i] = cola.poll();
+				cola.add(cola.poll());
+				i++;
+			}
+			System.out.println("Discarded cards:" + Arrays.toString(vec).replaceAll("\\[", " ").replaceAll("\\]", ""));
+			System.out.println("Remaining card: " + cola.peek());
+		}
 	}
 }
